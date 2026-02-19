a384f79b01e07b5890b5095c686a957dc1926810
ayushhu16 <ayush.hurloll@gmail.com>
2026-02-19 21:12:57 +0400

    app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 from flask import Flask, request, jsonify
-from scraper import AmazonAPI
+from scraper import MediaSpaceAPI
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Amazon Scraper API Running"
+    return "Mediaspace Scraper API Running"
 
 @app.route("/scrape")
 def scrape():
-    search = request.args.get("search", "PS5")
+    search = request.args.get("search", "")
     min_price = request.args.get("min", 0)
     max_price = request.args.get("max", 999999)
 
-    scraper = AmazonAPI(search, min_price, max_price)
+    scraper = MediaSpaceAPI(search, min_price, max_price)
     data = scraper.scrape()
-
     return jsonify(data)
