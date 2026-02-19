baac1b53c0b7be0b1d8c44acf4fb53275e315b95
ayushhu16 <ayush.hurloll@gmail.com>
2026-02-19 20:48:18 +0400

    app.py

--- app.py
+++ app.py
@@ -1,37 +1,19 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
 from scraper import AmazonAPI
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Amazon Scraper API is running!"
+    return "Amazon Scraper API Running"
 
 @app.route("/scrape")
 def scrape():
-    search = request.args.get("search", "PS4")
-    min_price = request.args.get("min", "275")
-    max_price = request.args.get("max", "650")
+    search = request.args.get("search", "PS5")
+    min_price = request.args.get("min", 0)
+    max_price = request.args.get("max", 999999)
 
     scraper = AmazonAPI(search, min_price, max_price)
-    data = scraper.run()
+    data = scraper.scrape()
 
-    best_item = None
-    if data:
-        valid_prices = [p for p in data if p["price"]]
-        if valid_prices:
-            best_item = min(valid_prices, key=lambda x: x["price"])
-
-    return jsonify({
-        "search": search,
-        "filters": {
-            "min": min_price,
-            "max": max_price
-        },
-        "best_item": best_item,
-        "products": data
-    })
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    return jsonify(data)
