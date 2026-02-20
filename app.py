e261cf3a1e24105ad44b5f7b164ba7b82f7e0c21
ayushhu16 <ayush.hurloll@gmail.com>
2026-02-20 15:11:34 +0400

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from scraper import MediaSpaceAPI
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Server is LIVE"
+
+@app.route("/scrape")
+def scrape():
+    search = request.args.get("search", "")
+    min_price = request.args.get("min", 0)
+    max_price = request.args.get("max", 999999)
+
+    scraper = MediaSpaceAPI(search, min_price, max_price)
+    data = scraper.scrape()
+
+    return jsonify(data)
